e60d8ee45140529726720e8962b06e7536cc034b
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-07 10:51:01 -0600

    Create tasks#update route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show'
   get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
 end
