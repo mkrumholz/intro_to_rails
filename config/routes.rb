0a57396c5dd81d046733ebb95bb36976353ff279
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-07 10:30:00 -0600

    Add tasks#show route

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
+  get '/tasks/id', to: 'tasks#show'
 end
