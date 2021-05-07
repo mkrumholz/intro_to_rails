85ccbb92c557e5ff98ade71a07f908d01c0b427a
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-07 10:34:42 -0600

    Create task page view and update route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
-  get '/tasks/id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show'
 end
