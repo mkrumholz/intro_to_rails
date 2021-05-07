344eb0ccb46bab4e13ca85bc557943db78935fbd
Molly Krumholz <mkkrumholz@gmail.com>
2021-05-07 11:01:15 -0600

    Create tasks#destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get '/tasks/:id', to: 'tasks#show'
   get '/tasks/:id/edit', to: 'tasks#edit'
   patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
